ea6ccec33e4f92d52dc6f58e0825e8bb6971d360
Anzhelika Novikova <anzhelika.novikova93@gmail.com>
2023-01-28 19:17:28 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2) return false;
+
+  let res = str;
+
+  for(let i = 0; i < res.length; i++) {
+    for(let j = 0; j < bracketsConfig.length; j++) {
+      const [op, cl] = bracketsConfig[j];
+
+      if (res[i] === op && res[i+1] === cl) {
+        res = res.replace(`${op}${cl}`, '');
+        i -= 2;
+      }
+    }
+  }
+
+  return !res.length
 }
